fe1e3fd9f6deafdf4cd008efe2fbc2c5023f9865
Aman Agrawal <aman9893089064@gmail.com>
2022-04-16 21:51:24 +0530

    Connectection created with DbContext

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //It will create a Category table with name Categories with four columns present in category
+        public DbSet<Category> Categories { get; set; }
+    }
+}

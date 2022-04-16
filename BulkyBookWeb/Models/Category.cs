f1b2ad675fcb42f7a505bbf5c22d60550b4c69f1
Aman Agrawal <aman9893089064@gmail.com>
2022-04-16 16:09:00 +0530

    Added Connection String

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,6 +1,17 @@
-﻿namespace BulkyBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
 }
